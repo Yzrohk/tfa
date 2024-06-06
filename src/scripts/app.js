04654c1b4b900aafbc0b62108407f38ca304a46e
Yzrohk <mdlol2011@hotmail.fr>
2024-06-06 02:18:41 +0200

    continuation CSS + ajout JS

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+function burgerMenu() {
+    var boutonBurgerMenu = document.querySelector(".burgermenu");
+    var listeSvgBurgermenu = document.querySelectorAll(".burgermenu__svg");
+    var menuListe = document.querySelector(".nav__menu");
+  
+    if (boutonBurgerMenu && listeSvgBurgermenu && menuListe) {
+      var activateBurgermenu = function activateBurgermenu() {
+        listeSvgBurgermenu.forEach(function (svg) {
+          if (svg.classList.contains("svg--active")) {
+            svg.classList.toggle("svg--inactive");
+            svg.classList.toggle("svg--active");
+          } else if (svg.classList.contains("svg--inactive")) {
+            svg.classList.toggle("svg--active");
+            svg.classList.toggle("svg--inactive");
+          }
+        });
+        menuListe.classList.toggle("nav__menu--active");
+      };
+  
+      boutonBurgerMenu.addEventListener("click", activateBurgermenu);
+    }
+  }
+  
+  burgerMenu();
